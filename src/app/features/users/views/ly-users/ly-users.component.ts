fb4f53041b6a5fc7ac8b002364e1bf8cbc8a6d3c
Wataru.Kasahara <kasahara1019@gmail.com>
2023-01-14 19:43:17 +0900

    npm run ng g c features/users/views/ly-users

---
+++ src/app/features/users/views/ly-users/ly-users.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-ly-users',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './ly-users.component.html',
+  styleUrls: ['./ly-users.component.scss']
+})
+export class LyUsersComponent {
+
+}
