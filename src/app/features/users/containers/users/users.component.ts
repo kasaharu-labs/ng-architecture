9dd7de2e1d6f4ab9bc3fa10aa988aeb1f8ffe0af
Wataru.Kasahara <kasahara1019@gmail.com>
2023-01-16 08:47:23 +0900

    feat: 取得した users を表示

--- src/app/features/users/containers/users/users.component.ts
+++ src/app/features/users/containers/users/users.component.ts
@@ -2,17 +2,21 @@ import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersUsecase } from './users.usecase';
 import { UsersStore } from './users.store';
+import { LyUsersComponent } from '../../views/ly-users/ly-users.component';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LyUsersComponent],
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss'],
   providers: [UsersStore, UsersUsecase],
 })
 export class UsersComponent implements OnInit {
+  private readonly store = inject(UsersStore);
   private readonly usecase = inject(UsersUsecase);
+
+  readonly users$ = this.store.users$;
 
   ngOnInit(): void {
     this.usecase.init();
--- src/app/features/users/views/ly-users/ly-users.component.ts
+++ src/app/features/users/views/ly-users/ly-users.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { User } from '../../../../domain/user';
 
 @Component({
   selector: 'app-ly-users',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './ly-users.component.html',
-  styleUrls: ['./ly-users.component.scss']
+  styleUrls: ['./ly-users.component.scss'],
 })
 export class LyUsersComponent {
-
+  @Input() users: User[] | null = null;
 }
