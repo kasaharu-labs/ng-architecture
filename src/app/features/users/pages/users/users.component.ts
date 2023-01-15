cf7fb74babfa48deefcbaf0e8016e3c54b0b8896
Wataru.Kasahara <kasahara1019@gmail.com>
2023-01-15 12:00:13 +0900

    feat: UsersPageComponent にタイトルを表示

--- src/app/features/users/pages/users/users.component.ts
+++ src/app/features/users/pages/users/users.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersComponent } from '../../containers/users/users.component';
+import { PageTitleComponent } from '../../../../shared/page-title/page-title.component';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, UsersComponent],
+  imports: [CommonModule, PageTitleComponent, UsersComponent],
   templateUrl: './users.component.html',
 })
 export default class UsersPageComponent {}
