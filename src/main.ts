ac88b41e837464b8c326e6376fa7eedc187d3aae
Wataru.Kasahara <kasahara1019@gmail.com>
2023-01-15 12:56:30 +0900

    npm run ng add @angular/material

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,7 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/routes';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { importProvidersFrom } from '@angular/core';
 
-bootstrapApplication(AppComponent, { providers: [provideRouter(routes), provideHttpClient()] });
+bootstrapApplication(AppComponent, { providers: [provideRouter(routes), provideHttpClient(), importProvidersFrom(BrowserAnimationsModule)] });
