3435987ef80c47a25227df4708a9e5177521d1d2
Wataru.Kasahara <kasahara1019@gmail.com>
2023-01-17 21:16:28 +0900

    feat: ユーザー一覧をテーブルで表示

--- src/app/features/users/views/ly-users/ly-users.component.ts
+++ src/app/features/users/views/ly-users/ly-users.component.ts
@@ -1,14 +1,21 @@
-import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
 import { User } from '../../../../domain/user';
 
 @Component({
   selector: 'app-ly-users',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatTableModule],
   templateUrl: './ly-users.component.html',
   styleUrls: ['./ly-users.component.scss'],
 })
 export class LyUsersComponent {
   @Input() users: User[] | null = null;
+  displayedColumns: string[] = ['name', 'username', 'email'];
+
+  click(user: User): void {
+    console.log('click ----- ');
+    console.log(user);
+  }
 }
