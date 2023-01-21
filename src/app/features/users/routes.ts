b3dbd5f8e1324687571ffd966a3cffdd87cf1a42
Wataru.Kasahara <kasahara1019@gmail.com>
2023-01-21 19:55:29 +0900

    chore: users feature の routes を活かすように変更

--- src/app/features/users/routes.ts
+++ src/app/features/users/routes.ts
@@ -1,4 +1,8 @@
 import { Route } from '@angular/router';
+import NewUserPageComponent from './pages/new-user/new-user.component';
 import UsersPageComponent from './pages/users/users.component';
 
-export const routes: Route[] = [{ path: '', pathMatch: 'full', component: UsersPageComponent }];
+export const routes: Route[] = [
+  { path: '', pathMatch: 'full', component: UsersPageComponent },
+  { path: 'new', component: NewUserPageComponent },
+];
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/users', pathMatch: 'full' },
-  { path: 'users', loadComponent: () => import('./features/users/pages/users/users.component') },
-  { path: 'users/new', loadComponent: () => import('./features/users/pages/new-user/new-user.component') },
+  // NOTE: loadComponent を使うと feature の routes.ts も不要になる
+  // { path: 'users', loadComponent: () => import('./features/users/pages/users/users.component') },
+  // { path: 'users/new', loadComponent: () => import('./features/users/pages/new-user/new-user.component') },
+  { path: 'users', loadChildren: () => import('./features/users/routes').then((m) => m.routes) },
 ];
