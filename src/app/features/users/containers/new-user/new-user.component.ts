d11e4612f101c96a2ed8afea0bfd17f930aef6cc
Wataru.Kasahara <kasahara1019@gmail.com>
2023-01-21 16:38:48 +0900

    npm run ng g c features/users/containers/new-user

---
+++ src/app/features/users/containers/new-user/new-user.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-new-user',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './new-user.component.html',
+  styleUrls: ['./new-user.component.scss']
+})
+export class NewUserComponent {
+
+}
