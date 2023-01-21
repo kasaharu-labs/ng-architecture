fc63d39befa7e7162822a7f8c35f33db43a15e72
Wataru.Kasahara <kasahara1019@gmail.com>
2023-01-21 16:37:58 +0900

    refactor: page component から selector と styleUrls を削除

--- src/app/features/users/pages/new-user/new-user.component.ts
+++ src/app/features/users/pages/new-user/new-user.component.ts
@@ -2,10 +2,8 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-new-user',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './new-user.component.html',
-  styleUrls: ['./new-user.component.scss'],
 })
 export class NewUserPageComponent {}
