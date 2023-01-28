6b48b1375bdd41bd699a3fabe6c78b10f9defe5d
Wataru.Kasahara <kasahara1019@gmail.com>
2023-01-28 12:24:05 +0900

    feat: page component で保存した state を container component で購読

--- src/app/features/users/containers/user-detail/user-detail.component.ts
+++ src/app/features/users/containers/user-detail/user-detail.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LyUserDetailComponent } from '../../views/ly-user-detail/ly-user-detail.component';
+import { UserDetailPageStore } from '../../pages/user-detail/user-detail.page-store';
+import { takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-user-detail',
@@ -9,4 +11,13 @@ import { LyUserDetailComponent } from '../../views/ly-user-detail/ly-user-detail
   templateUrl: './user-detail.component.html',
   styleUrls: ['./user-detail.component.scss'],
 })
-export class UserDetailComponent {}
+export class UserDetailComponent implements OnInit {
+  private readonly pageStore = inject(UserDetailPageStore);
+  private readonly userId$ = this.pageStore.userId$;
+
+  ngOnInit(): void {
+    this.userId$.pipe(takeUntil(this.pageStore.destroy$)).subscribe((userId) => {
+      console.log(userId);
+    });
+  }
+}
