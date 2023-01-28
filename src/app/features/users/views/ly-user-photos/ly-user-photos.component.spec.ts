e567a72c4e8754d3d2f6f023861e10a44bf6c671
Wataru.Kasahara <kasahara1019@gmail.com>
2023-01-28 13:39:52 +0900

    npm run ng g c features/users/views/ly-user-photos

---
+++ src/app/features/users/views/ly-user-photos/ly-user-photos.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LyUserPhotosComponent } from './ly-user-photos.component';
+
+describe('LyUserPhotosComponent', () => {
+  let component: LyUserPhotosComponent;
+  let fixture: ComponentFixture<LyUserPhotosComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ LyUserPhotosComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(LyUserPhotosComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/features/users/views/ly-user-photos/ly-user-photos.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-ly-user-photos',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './ly-user-photos.component.html',
+  styleUrls: ['./ly-user-photos.component.scss']
+})
+export class LyUserPhotosComponent {
+
+}
