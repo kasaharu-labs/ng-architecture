6eb18f1ff895d749bc85c33b5bab2307540c7935
Wataru.Kasahara <kasahara1019@gmail.com>
2023-01-28 11:23:20 +0900

    feat: 任意の album の photo 一覧を取得する api method を実装

--- src/app/infrastructures/api/photo.api.ts
+++ src/app/infrastructures/api/photo.api.ts
@@ -1,6 +1,17 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Photo } from '../../domain/photo';
+import { AppConfig } from '../../providers/app-config';
 
 @Injectable({
   providedIn: 'root',
 })
-export class PhotoApi {}
+export class PhotoApi {
+  private readonly appConfig = inject(AppConfig);
+  private readonly http = inject(HttpClient);
+
+  getPhotos(albumId: number): Observable<Photo[]> {
+    return this.http.get<Photo[]>(`${this.appConfig.apiUrl}/photos?albumId=${albumId}`);
+  }
+}
