e74a5153115c6220a819a9b687367640e08a5db4
Wataru.Kasahara <kasahara1019@gmail.com>
2023-01-28 12:14:10 +0900

    feat: container component から presentational component を表示

--- src/app/features/users/containers/user-detail/user-detail.component.ts
+++ src/app/features/users/containers/user-detail/user-detail.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LyUserDetailComponent } from '../../views/ly-user-detail/ly-user-detail.component';
 
 @Component({
   selector: 'app-user-detail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LyUserDetailComponent],
   templateUrl: './user-detail.component.html',
   styleUrls: ['./user-detail.component.scss'],
 })
