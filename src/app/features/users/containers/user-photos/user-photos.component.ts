34c9cf84c3473d8692c5796195fc96ead2bbd541
Wataru.Kasahara <kasahara1019@gmail.com>
2023-01-28 13:36:54 +0900

    npm run ng g c features/users/containers/user-photos

---
+++ src/app/features/users/containers/user-photos/user-photos.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-user-photos',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './user-photos.component.html',
+  styleUrls: ['./user-photos.component.scss']
+})
+export class UserPhotosComponent {
+
+}
