19b8ca54fc43c40e50a20b8aa092a582ec4d9802
Wataru.Kasahara <kasahara1019@gmail.com>
2023-01-28 11:28:24 +0900

    feat: page から container を表示

--- src/app/features/users/pages/user-detail/user-detail.component.ts
+++ src/app/features/users/pages/user-detail/user-detail.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserDetailComponent } from '../../containers/user-detail/user-detail.component';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, UserDetailComponent],
   templateUrl: './user-detail.component.html',
 })
 export default class UserDetailPageComponent {}
