5d81205e81a688f20d5babdb4cd1f80ac6e65f24
Wataru.Kasahara <kasahara1019@gmail.com>
2023-01-28 11:19:37 +0900

    feat: 任意の user の album 一覧を取得する api method を実装

--- src/app/infrastructures/api/album.api.ts
+++ src/app/infrastructures/api/album.api.ts
@@ -1,6 +1,17 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Album } from '../../domain/album';
+import { AppConfig } from '../../providers/app-config';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AlbumApi {}
+export class AlbumApi {
+  private readonly appConfig = inject(AppConfig);
+  private readonly http = inject(HttpClient);
+
+  getAlbums(userId: number): Observable<Album[]> {
+    return this.http.get<Album[]>(`${this.appConfig.apiUrl}/albums?userId=${userId}`);
+  }
+}
