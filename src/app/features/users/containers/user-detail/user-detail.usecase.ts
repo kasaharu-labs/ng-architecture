260023c99777eee82bb129bae2acd3e3d12bf531
Wataru.Kasahara <kasahara1019@gmail.com>
2023-01-28 12:44:26 +0900

    feat: ユーザー詳細を開いたときに対象の user を取得する

--- src/app/features/users/containers/user-detail/user-detail.usecase.ts
+++ src/app/features/users/containers/user-detail/user-detail.usecase.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+import { UserApi } from '../../../../infrastructures/api/user.api';
+import { UserDetailStore } from './user-detail.store';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable()
 export class UserDetailUsecase {
-  constructor() {}
+  private readonly userApi = inject(UserApi);
+  private readonly store = inject(UserDetailStore);
+
+  async init(userId: number): Promise<void> {
+    const user = await firstValueFrom(this.userApi.getUser(userId));
+    this.store.setUser(user);
+  }
 }
