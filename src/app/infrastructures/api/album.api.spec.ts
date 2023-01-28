20d6ce35e1fbe27446f3c1e80bdae6e6ee4dbe3e
Wataru.Kasahara <kasahara1019@gmail.com>
2023-01-28 17:58:48 +0900

    test: api テストの依存を解決

--- src/app/infrastructures/api/album.api.spec.ts
+++ src/app/infrastructures/api/album.api.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { AlbumApi } from './album.api';
@@ -6,7 +7,9 @@ describe('AlbumApi', () => {
   let api: AlbumApi;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     api = TestBed.inject(AlbumApi);
   });
 
--- src/app/infrastructures/api/photo.api.spec.ts
+++ src/app/infrastructures/api/photo.api.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { PhotoApi } from './photo.api';
@@ -6,7 +7,9 @@ describe('PhotoApi', () => {
   let api: PhotoApi;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     api = TestBed.inject(PhotoApi);
   });
 
