6c127053970b899e6a996f759aa653d4566bd7f2
Wataru.Kasahara <kasahara1019@gmail.com>
2023-01-31 22:13:10 +0900

    test: 依存している api class を providers に追加

--- src/app/features/users/pages/user-detail/user-detail.component.spec.ts
+++ src/app/features/users/pages/user-detail/user-detail.component.spec.ts
@@ -1,8 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
 import { ActivatedRouteStub } from '../../../../../../testing/activated-route-stub';
+import { AlbumApi } from '../../../../infrastructures/api/album.api';
+import { PhotoApi } from '../../../../infrastructures/api/photo.api';
+import { UserApi } from '../../../../infrastructures/api/user.api';
+import { UserDetailComponent } from '../../containers/user-detail/user-detail.component';
+import { UserPhotosComponent } from '../../containers/user-photos/user-photos.component';
 
 import UserDetailPageComponent from './user-detail.component';
+
+jest.mock('../../../../infrastructures/api/album.api');
+jest.mock('../../../../infrastructures/api/photo.api');
+jest.mock('../../../../infrastructures/api/user.api');
 
 describe('UserDetailPageComponent', () => {
   let component: UserDetailPageComponent;
@@ -13,8 +22,8 @@ describe('UserDetailPageComponent', () => {
     activatedRoute = new ActivatedRouteStub({ id: 1 });
 
     await TestBed.configureTestingModule({
-      imports: [UserDetailPageComponent],
-      providers: [{ provide: ActivatedRoute, useValue: activatedRoute }],
+      imports: [UserDetailPageComponent, UserDetailComponent, UserPhotosComponent],
+      providers: [UserApi, AlbumApi, PhotoApi, { provide: ActivatedRoute, useValue: activatedRoute }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(UserDetailPageComponent);
