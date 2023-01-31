7372f70054d962574078c942687cac18004a65fc
Wataru.Kasahara <kasahara1019@gmail.com>
2023-01-31 21:51:51 +0900

    test: ActivatedRoute の stub を用意し依存を解決

--- src/app/features/users/pages/user-detail/user-detail.component.spec.ts
+++ src/app/features/users/pages/user-detail/user-detail.component.spec.ts
@@ -1,14 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { ActivatedRouteStub } from '../../../../../../testing/activated-route-stub';
 
 import UserDetailPageComponent from './user-detail.component';
 
 describe('UserDetailPageComponent', () => {
   let component: UserDetailPageComponent;
   let fixture: ComponentFixture<UserDetailPageComponent>;
+  let activatedRoute: ActivatedRouteStub;
 
   beforeEach(async () => {
+    activatedRoute = new ActivatedRouteStub({ id: 1 });
+
     await TestBed.configureTestingModule({
       imports: [UserDetailPageComponent],
+      providers: [{ provide: ActivatedRoute, useValue: activatedRoute }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(UserDetailPageComponent);
