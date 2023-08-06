2c2400348f3567a4913e473ecace46735b6137e0
Wataru.Kasahara <kasahara1019@gmail.com>
2023-08-06 18:55:04 +0900

    test: RouterTestingHarness を使ったテストに変更

--- src/app/features/users/pages/users/users.component.spec.ts
+++ src/app/features/users/pages/users/users.component.spec.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+import { RouterTestingHarness } from '@angular/router/testing';
+import { routes } from '../../../../routes';
 import { PageTitleComponent } from '../../../../shared/page-title/page-title.component';
 import { UsersComponent } from '../../containers/users/users.component';
 import UsersPageComponent from './users.component';
@@ -8,19 +11,20 @@ import UsersPageComponent from './users.component';
 class MockUsersComponent {}
 
 describe('UsersComponent', () => {
+  let harness: RouterTestingHarness;
   let component: UsersPageComponent;
-  let fixture: ComponentFixture<UsersPageComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [UsersPageComponent, PageTitleComponent],
+      providers: [provideRouter(routes)],
     })
       .overrideComponent(UsersPageComponent, { remove: { imports: [UsersComponent] }, add: { imports: [MockUsersComponent] } })
       .compileComponents();
 
-    fixture = TestBed.createComponent(UsersPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    harness = await RouterTestingHarness.create();
+    component = await harness.navigateByUrl('', UsersPageComponent);
+    harness.detectChanges();
   });
 
   it('should create', () => {
