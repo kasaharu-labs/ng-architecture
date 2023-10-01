ea53323085801aee3fb51ec6ec9fcc4faf081729
Wataru.Kasahara <kasahara1019@gmail.com>
2023-10-01 12:47:00 +0900

    refactor: 不要になった store を削除

--- src/app/features/users/pages/user-detail/user-detail.component.ts
+++ src/app/features/users/pages/user-detail/user-detail.component.ts
@@ -4,17 +4,14 @@ import { ActivatedRoute } from '@angular/router';
 import { PageTitleComponent } from '../../../../shared/page-title/page-title.component';
 import { UserDetailComponent } from '../../containers/user-detail/user-detail.component';
 import { UserPhotosComponent } from '../../containers/user-photos/user-photos.component';
-import { UserDetailPageStore } from './user-detail.page-store';
 
 @Component({
   standalone: true,
   imports: [CommonModule, UserDetailComponent, UserPhotosComponent, PageTitleComponent],
   templateUrl: './user-detail.component.html',
-  providers: [UserDetailPageStore],
 })
 export default class UserDetailPageComponent implements OnInit {
   private readonly route = inject(ActivatedRoute);
-  private readonly store = inject(UserDetailPageStore);
 
   userId: number | null = null;
 
@@ -26,6 +23,5 @@ export default class UserDetailPageComponent implements OnInit {
       alert('ユーザーの ID が不正です');
       return;
     }
-    this.store.setUserId(this.userId);
   }
 }
